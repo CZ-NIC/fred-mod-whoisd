0e95e2105a2ec1e2b6060559fa3259235b160bbc
Ondřej Surý <ondrej.sury@nic.cz>
2007-09-27 09:27:38 +0000

    add or modify prevent.sh scripts
    git-svn-id: file:///data/svn/enum/mod_whoisd/trunk@4157 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- prevent.sh
+++ prevent.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-cov-build --dir ~/cov/mod_whoisd/ --config /opt/prevent-linux64-3.6.1/config/coverity_config.xml make && \
-cov-analyze --all --dir ~/cov/mod_whoisd/ --config /opt/prevent-linux64-3.6.1/config/coverity_config.xml && \
-cov-commit-defects --datadir ~/cov/gui/ --product mod_whoisd --user ondrej --dir ~/cov/mod_whoisd/
+PRODUCT=`basename \`dirname \\\`pwd\\\`\``
+DIR=~/cov/${PRODUCT}/
+cov-build --dir ${DIR} --config /opt/prevent-linux64-3.6.1/config/coverity_config.xml make && \
+cov-analyze --all --dir ${DIR} --config /opt/prevent-linux64-3.6.1/config/coverity_config.xml && \
+cov-commit-defects --datadir ~/cov/gui/ --product ${PRODUCT} --user ondrej --dir ${DIR}
